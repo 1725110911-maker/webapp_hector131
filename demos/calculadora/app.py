abf2571ba0036833003cccd18d4b13bb2fc8b309
HECTOR EMILIANO CRUZ RAMIREZ <1725110911@utectulancingo.edu.mx>
2026-06-12 20:19:01 +0000

    UPDATED calculadora

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -5,9 +5,9 @@ urls = (
     '/calculadora', 'Calculadora'
 )
 
-app = web.application(urls, globals())
 render = web.template.render('views')
 
+app = web.application(urls, globals())
 
 class Index:
     def GET(self):
@@ -15,20 +15,54 @@ class Index:
 
 class Calculadora:
     def GET(self):
-        numero_1 = 0.0
-        numero_2 = 0.0
-        resultado = 0.0
+        numero_1=0.0
+        numero_2=0.0
+        resultado=0.0
         return render.calculadora(numero_1, numero_2, resultado)
-    
+
     def POST(self):
         formulario = web.input()
         numero_1 = float(formulario['numero_1'])
         numero_2 = float(formulario['numero_2'])
-        resultado = numero_1 + numero_2
+        operacion = formulario['operacion']
+        resultado = 0.0
 
-        print(f"Tipo de dato de numero_1 : {type(numero_1)}")
-        print(f"Tipo de dato de numero_2 : {type(numero_2)}")
+        print(f"Tipo de dato de numero_1: {type(numero_1)}")
+        print(f"Tipo de dato de numero_2: {type(numero_2)}")
+
+        if operacion == 'sumar':
+            resultado = numero_1 + numero_2
+
+        elif operacion == 'restar':
+            resultado = numero_1 - numero_2
+            
+        elif operacion == 'multiplicar':
+             resultado = numero_1 * numero_2
+
+        elif operacion == 'dividir':
+            if numero_2 == 0:
+             resultado = numero_1 / numero_2
+            else:
+                resultado = "Error: división entre cero"
+
+        elif operacion == 'modulo':
+            resultado = numero_1 % numero_2
+
+        elif operacion == 'raiz_cuadrada':
+            if numero_1 >= 0:
+                resultado = numero_1 ** 0.5
+
+        elif operacion == 'potencia':
+            resultado = numero_1 ** numero_2
+
+        elif operacion == 'limpiar':
+            numero_1 = 0.0
+            numero_2 = 0.0
+            resultado = 0.0
+        else:
+            resultado = "Error: número negativo"
+
         return render.calculadora(numero_1, numero_2, resultado)
-   
+    
 if __name__ == "__main__":
     app.run()
